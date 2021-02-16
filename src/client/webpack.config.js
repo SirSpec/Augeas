6ad98c797626d181fc0cce1d9f40616494b7fc61
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-16 17:24:04 +0100

    Add build mode in webpack config

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
