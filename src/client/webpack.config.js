1e96e98eaaf26235dcad164913a5ec4bde407913
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-17 00:05:05 +0100

    Add webpack configuration to support react, redux and sass

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -4,14 +4,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-    ],
     entry: {
         main: './src/index.js',
     },
@@ -20,4 +12,42 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        }
+                    },
+                    'sass-loader',
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+    ],
 };
