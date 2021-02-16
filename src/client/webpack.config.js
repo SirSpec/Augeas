96e5918b712d581309c60cbba10cfd508acb84ff
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-16 11:48:34 +0100

    Add html-webpack-plugin to webpack config

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+    ],
     entry: {
         main: './src/index.js',
     },
