e5f291af93dab9200f5b5de760b544de5d495310
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-02 22:14:05 +0100

    Add Layers parameter in NeuralNetwork

--- src/server/Domain/ArtificialIntelligence/NeuralNetwork.cs
+++ src/server/Domain/ArtificialIntelligence/NeuralNetwork.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+
 namespace Hermes.Domain.ArtificialIntelligence
 {
     // https://en.wikipedia.org/wiki/Neural_network
     public class NeuralNetwork
     {
-
+        public IEnumerable<NeuronLayer> Layers { get; }
     }
 }
