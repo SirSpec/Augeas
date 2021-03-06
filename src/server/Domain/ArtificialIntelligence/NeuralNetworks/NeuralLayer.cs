6b1b7692e7541f199210cb7f5a6cdca7bce2d962
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-06 19:12:32 +0100

    Add implementation of NeuralLayer

---
+++ src/server/Domain/ArtificialIntelligence/NeuralNetworks/NeuralLayer.cs
@@ -0,0 +1,12 @@
+namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
+{
+    public class NeuralLayer
+    {
+        private Neuron[] Neurons;
+        
+        public NeuralLayer(Neuron[] neurons)
+        {
+            Neurons = neurons;
+        }
+    }
+}
