4fa2aa5b4088c35417f2a47a9283d508ce7b8f68
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-06 18:53:24 +0100

    Add implementation of Connection

---
+++ src/server/Domain/ArtificialIntelligence/NeuralNetworks/Connection.cs
@@ -0,0 +1,16 @@
+namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
+{
+    public class Connection
+    {
+        private readonly INeuron connectedNeuron;
+
+        public Connection(INeuron connectedNeuron, double weight)
+        {
+            this.connectedNeuron = connectedNeuron;
+            Weight = weight;
+        }
+
+        public double InputValue => connectedNeuron.Output;
+        public double Weight { get; set; }
+    }
+}
