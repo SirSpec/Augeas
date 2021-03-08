5d235f58a9a5251060b7817a1000c612762cd986
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-08 15:01:56 +0100

    Extract Actor class from PlaygroundService

---
+++ src/client/src/components/Playground/Actor.js
@@ -0,0 +1,48 @@
+import Phaser from 'phaser';
+
+export default class Actor {
+    constructor(x, y, angle, actorObject, sensors) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.isAlive = true;
+
+        this.actorObject = actorObject;
+        this.actorObject.setPosition(this.x, this.y);
+        this.sensors = sensors;
+        this.setSensorsPosition();
+    }
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+        this.actorObject.setPosition(this.x, this.y);
+    }
+
+    rotate(angle) {
+        this.angle = angle;
+        this.setSensorsPosition();
+    }
+
+    getAngleWithOffset(offset) {
+        return (offset + this.angle) * Math.PI / 180;
+    }
+
+    translateActor(translationVector) {
+        translationVector.setAngle(this.getAngleWithOffset(0));
+        Phaser.Geom.Circle.Offset(this.actorObject, translationVector.x, translationVector.y);
+
+        this.sensors.forEach(sensor => {
+            Phaser.Geom.Line.Offset(sensor.sensorLine, translationVector.x, translationVector.y);
+        });
+
+        this.x = this.actorObject.x;
+        this.y = this.actorObject.y;
+    }
+
+    setSensorsPosition() {
+        this.sensors.forEach(sensor => {
+            Phaser.Geom.Line.SetToAngle(sensor.sensorLine, this.x, this.y, sensor.getAngle(this.angle), sensor.width);
+        });
+    }
+}
