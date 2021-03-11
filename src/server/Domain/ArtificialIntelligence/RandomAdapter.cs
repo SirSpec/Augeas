b6ee591e17ed61918ce30f6fd0b3def9f073bf13
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-11 21:13:45 +0100

    Add RandomAdapter implementation

---
+++ src/server/Domain/ArtificialIntelligence/RandomAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hermes.Domain.ArtificialIntelligence
+{
+    public class RandomAdapter : IRandomNumberEngine
+    {
+        private readonly Random random;
+
+        public RandomAdapter() =>
+            random = new Random();
+
+        public RandomAdapter(int seed) =>
+            random = new Random(seed);
+
+        public int RandomInteger(ConstraintRange<int> constraint) =>
+            random.Next(constraint.Min, constraint.Max);
+
+        public double RandomDouble(ConstraintRange<double> constraint) =>
+            (random.NextDouble() * (constraint.Max - constraint.Min)) + constraint.Min;
+    }
+}
