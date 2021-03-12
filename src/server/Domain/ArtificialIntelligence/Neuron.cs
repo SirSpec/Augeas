ce1de0be4f9a4f15a55c37b9ed8a9bd37651977f
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-12 12:03:41 +0100

    Delete redundant files

--- src/server/Domain/ArtificialIntelligence/Neuron.cs
+++
@@ -1,9 +0,0 @@
-namespace Hermes.Domain.ArtificialIntelligence
-{
-    public class Neuron
-    {
-        // activation _ value _ function
-        // Input function
-        // Output (activation) function
-    }
-}
