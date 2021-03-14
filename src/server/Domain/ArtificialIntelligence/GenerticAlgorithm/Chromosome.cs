560ad8fde2043dbc7959b9ce9660cb4e94a23f69
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-14 08:37:41 +0100

    Improve Chromosome implementation - Add Length and Genes properties - Add indexer implementation

--- src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/Chromosome.cs
+++ src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/Chromosome.cs
@@ -1,20 +1,24 @@
+using System.Collections.Generic;
+
 namespace Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm
 {
     // https://en.wikipedia.org/wiki/Chromosome_(genetic_algorithm)
     public class Chromosome<TAllele>
     {
         private readonly Gene<TAllele>[] genes;
+
         public Chromosome(params Gene<TAllele>[] genes) =>
             this.genes = genes;
 
-        public Chromosome(int size) : this(new Gene<TAllele>[size])
-        {
-        }
-
-        public Gene<TAllele> this[int index] =>
-            genes[index];
-
         public int Length =>
             genes.Length;
+
+        public TAllele this[int index]
+        {
+            get => genes[index].Allele;
+            set => genes[index].Allele = value;
+        }
+
+        public IEnumerable<Gene<TAllele>> Genes => genes;
     }
 }
