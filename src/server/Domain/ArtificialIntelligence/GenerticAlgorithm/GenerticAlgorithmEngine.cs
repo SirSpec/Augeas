b16df9d2cf23eb89c0e223d7e14fe9fdd3ac60b3
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-14 08:48:20 +0100

    Add test implementation of GenerticAlgorithmEngine

--- src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/GenerticAlgorithmEngine.cs
+++ src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/GenerticAlgorithmEngine.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm
@@ -10,14 +9,14 @@ namespace Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm
         private readonly IFitnessFunction fitnessFunction;
         private readonly ISelectionAlgorithm selectionAlgorithm;
         private readonly ICrossoverOperator crossoverOperator; //Alterer operators
-        private readonly IMutationOperator mutationOperator;
+        private readonly IMutationOperator<T> mutationOperator;
         private readonly ITerminationAlgorithm terminationAlgorithm;
 
         public GenerticAlgorithmEngine(
             IFitnessFunction fitnessFunction,
             ISelectionAlgorithm selectionAlgorithm,
             ICrossoverOperator crossoverOperator,
-            IMutationOperator mutationOperator,
+            IMutationOperator<T> mutationOperator,
             ITerminationAlgorithm terminationAlgorithm)
         {
             this.fitnessFunction = fitnessFunction;
@@ -37,27 +36,78 @@ namespace Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm
         //     Compute fitness
         // UNTIL population has converged
         // STOP
-        public Phenotype<T> Evolve(Population<T> population2)
+        // public Phenotype<T> Evolve(Population<T> population2)
+        // {
+        //     //initialize population
+        //     var population = population2; //new Population<T>(0, new Phenotype<T>(fitnessFunction, new Genotype<T>(), 0));
+
+        //     //find fitness of population
+        //     foreach (var phenotype in population.Phenotypes)
+        //     {
+        //         phenotype.Fitness = fitnessFunction.GetFitness(phenotype);
+        //     }
+
+        //     while (terminationAlgorithm.TerminationConditionReached(population) is false)
+        //     {
+        //         var parents = selectionAlgorithm.Select(population.Phenotypes);
+
+        //         var offsprings = new List<Phenotype<T>>();
+
+        //         foreach (var pair in parents)
+        //         {
+        //             var offspring = crossoverOperator.Crossover(parents);
+        //             offspring = offspring.Select(mutationOperator.Mutate);
+        //             offsprings.AddRange(offspring);
+        //         }
+
+        //         foreach (var phenotype in offsprings)
+        //         {
+        //             phenotype.Fitness = fitnessFunction.GetFitness(phenotype);
+        //         }
+
+        //         var survivors = selectionAlgorithm.Select(parents).Concat(selectionAlgorithm.Select(offsprings));
+        //         population = population.CreateNextGeneration(survivors.ToArray());
+        //     }
+
+        //     return population.Fittest;
+        // }
+
+        public IEnumerable<(Phenotype<T> f, Phenotype<T> s)> GetPair(Phenotype<T>[] parents)
         {
-            //initialize population
-            var population = population2; //new Population<T>(0, new Phenotype<T>(fitnessFunction, new Genotype<T>(), 0));
-
-            //find fitness of population
-            // fitnessFunction.GetFitness(genotype);
-            while (terminationAlgorithm.TerminationConditionReached(population) is false)
+            for (int i = 0; i < parents.Length - 1; i += 2)
             {
-                var parents = selectionAlgorithm.Select(population.Phenotypes);
-                var offspring = crossoverOperator.Crossover(parents);
-                offspring = offspring.Select(mutationOperator.Mutate);
+                yield return (parents[i], parents[i + 1]);
+            }
+        }
 
-                //decode and fitness calculation
-                // fitnessFunction.GetFitness(genotype);
+        public Population<T> Generate(Population<T> population)
+        {
+            var newPopulation = population;
 
-                var survivors = selectionAlgorithm.Select(parents).Concat(selectionAlgorithm.Select(offspring));
-                population = population.CreateNextGeneration(survivors.ToArray());
+            while (terminationAlgorithm.TerminationConditionReached(newPopulation) is false)
+            {
+                var parents = selectionAlgorithm.Select(newPopulation.Phenotypes);
+
+                var newOffspring = new List<Phenotype<T>>();
+
+                foreach (var pair in GetPair(parents.ToArray()))
+                {
+                    var offspring = crossoverOperator.Crossover(new[] { pair.f, pair.s });
+                    offspring = offspring.Select(mutationOperator.Mutate);
+                    newOffspring.AddRange(offspring);
+                }
+
+                var survivors = parents.Concat(newOffspring)
+                    .Take(10)
+                    .ToArray();
+
+                return new Population<T>(
+                    population.Generation + 1,
+                    survivors
+                );
             }
 
-            return population.Fittest;
+            return population;
         }
     }
 }
