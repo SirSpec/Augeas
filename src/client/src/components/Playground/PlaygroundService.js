56c2fe078a9b76e4a376031fe2cbb79cdc80ecaf
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-16 09:05:48 +0100

    Refactor PlaygroundService - Remove redundant code - Improve code structure - Extract logic to external classes

--- src/client/src/components/Playground/PlaygroundService.js
+++ src/client/src/components/Playground/PlaygroundService.js
@@ -1,6 +1,5 @@
 import Phaser from 'phaser'
 import * as signalR from "@microsoft/signalr"
-import Sensor from './Sensor';
 import Actor from './Actor';
 import Map from './Map';
 
@@ -16,14 +15,13 @@ export default class PlaygroundService {
     }
 
     initialize() {
-        1
         var green = 0x00ff00;
         var red = 0xff0000;
         var yellow = "#ffff00";
 
         var connection = new signalR.HubConnectionBuilder()
             .withUrl("https://localhost:5001/hub")
-            .build()
+            .build();
 
         connection.on("ReceiveNewPopulation", () => {
             actors.forEach(actor => {
@@ -57,43 +55,21 @@ export default class PlaygroundService {
                 .catch(error => console.log(error.toString()));
         }
 
-        connection
-            .start()
+        connection.start()
             .then(() => console.log("Sudoku component connection started."))
-            .catch(error => console.log(error))
+            .catch(error => console.log(error));
 
         var graphics;
-
-        function getActor(id) {
-            return new Actor(id, 225, 80, 0, new Phaser.Geom.Circle(null, null, 10), [
-                new Sensor("Sensor 1", -90, new Phaser.Geom.Line(), new Phaser.Geom.Circle(null, null, 10)),
-                new Sensor("Sensor 2", -45, new Phaser.Geom.Line(), new Phaser.Geom.Circle(null, null, 10)),
-                new Sensor("Sensor 3", -15, new Phaser.Geom.Line(), new Phaser.Geom.Circle(null, null, 10)),
-                new Sensor("Sensor 4", 15, new Phaser.Geom.Line(), new Phaser.Geom.Circle(null, null, 10)),
-                new Sensor("Sensor 5", 45, new Phaser.Geom.Line(), new Phaser.Geom.Circle(null, null, 10)),
-                new Sensor("Sensor 6", 90, new Phaser.Geom.Line(), new Phaser.Geom.Circle(null, null, 10)),
-            ]);
-        }
-
-        var actors = [
-            getActor(0),
-            getActor(1),
-            getActor(2),
-            getActor(3),
-            getActor(4),
-            getActor(5),
-            getActor(6),
-            getActor(7),
-            getActor(8),
-            getActor(9),
-        ];
-
+        var actors;
         var text;
-        var walls = this.map.draw1();
+        var walls = this.map.getWalls();
         var finishLine = this.map.drawFinishLine();
+        var maxDistance = Phaser.Math.Distance.Between(
+            225, 80,
+            finishLine.x1, finishLine.y2 - (finishLine.y2 - finishLine.y1) / 2);
 
+        var keySpace;
         var keyUp;
-        var keyDown;
         var keyLeft;
         var keyRight;
 
@@ -110,26 +86,34 @@ export default class PlaygroundService {
 
         function create() {
             graphics = this.add.graphics();
-            createKeyboardInputs(this.input);
+            actors = [
+                Actor.create(0),
+                Actor.create(1),
+                Actor.create(2),
+                Actor.create(3),
+                Actor.create(4),
+                Actor.create(5),
+                Actor.create(6),
+                Actor.create(7),
+                Actor.create(8),
+                Actor.create(9),
+            ];
 
-            text = this.add.text(10, 810, '', { font: '16px Courier', fill: yellow });
+            keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+            keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+            keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+            keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+            text = this.add.text(10, 800, '', { font: '16px Courier', fill: yellow });
         }
 
         function update() {
-            var allDead = false;
-            for (let index = 0; index < actors.length; index++) {
-                const actor = actors[index];
-                if (actor.isAlive === false) {
-                    allDead = true;
-                }
-                else {
-                    allDead = false;
-                    break;
-                }
-            }
+            drawMap();
+            calculateCollisionPoints();
+            hangleKeyboardInputs();
+        }
 
-            if (allDead) generateNewPopulation();
-
+        function drawMap() {
             graphics.clear();
             graphics.lineStyle(2, green);
 
@@ -140,84 +124,19 @@ export default class PlaygroundService {
             graphics.lineStyle(1, 0xffff00);
             graphics.strokeLineShape(finishLine);
 
-            drawActor();
-
-            var currentDistance = Phaser.Math.Distance.Between(actors[0].x, actors[0].y, finishLine.x1, finishLine.y2 - (finishLine.y2 - finishLine.y1) / 2);
-            var maxDistance = Phaser.Math.Distance.Between(225, 80, finishLine.x1, finishLine.y2 - (finishLine.y2 - finishLine.y1) / 2);
-
             text.setText(
-                `${1 - currentDistance/maxDistance} Checkpoints: ${actors[0].fitness}|Alive:${actors[0].isAlive}\n` +
+                `Fitness: ${actors[0].fitness}|Alive:${actors[0].isAlive}\n` +
                 actors[0].sensors.map(sensor => `${sensor.toString()}`).join("\n"));
-
-            hangleKeyboardInputs();
-            countCheckpoints()
         }
 
-        function getDistance() {
-            var currentDistance = Phaser.Math.Distance.Between(actors[0].x, actors[0].y, finishLine.x1, finishLine.y1);
-            var maxDistance = Phaser.Math.Distance.Between(225, 80, finishLine.x1, finishLine.y1);
-
-            return currentDistance;//1 - (currentDistance / maxDistance);
-        }
-
-        function countCheckpoints() {
-            // for (let index = 0; index < checkpoints.length; index++) {
-            //     const checkpoint = checkpoints[index];
-
-            //     for (let j = 0; j < actors.length; j++) {
-            //         const actor = actors[j];
-            //         if (Phaser.Geom.Intersects.LineToCircle(checkpoint, actor.actorObject)) {
-            //             actor.fitness = (index + 1) / checkpoints.length;
-            //             break;
-            //         }
-            //     }
-            // }
-        }
-
-        function getDistance(d) {
-            return 1 - (d ?? 0.0);
-        }
-
-        function hangleKeyboardInputs() {
-
-            // actors.forEach(actor => {
-            //     if (actor.isAlive)
-            //         angle(actor.id, actor.sensors.map(sensor => getDistance(sensor.collisionCoordinate.distance)));
-            //     actor.setSensorsPosition();
-            // });
-
-            if (keyUp.isDown) {
-                // actors.forEach(actor => {
-                //     actor.isAlive = false;
-                // });
-                var translationVector = new Phaser.Math.Vector2(1, 1);
-                actors[0].translateActor(translationVector);
-            }
-            if (keyDown.isDown) {
-                var translationVector = new Phaser.Math.Vector2(-1, 1);
-                actors[0].translateActor(translationVector);
-            }
-            if (keyLeft.isDown) {
-                actors[0].rotate(actors[0].angle - 1);
-            }
-            if (keyRight.isDown) {
-                actors[0].rotate(actors[0].angle + 1);
-            }
-        }
-
-        function drawActor() {
+        function calculateCollisionPoints() {
             graphics.lineStyle(2, red);
-            actors.forEach(actor => {
-                graphics.strokeCircleShape(actor.actorObject);
-                actor.sensors.forEach(sensor => {
-                    graphics.strokeLineShape(sensor.sensorLine);
-                });
-            });
-            drawCollisionPoints();
-        }
 
-        function drawCollisionPoints() {
             actors.forEach(actor => {
+                if (actor.isAlive)
+                    angle(actor.id, actor.sensors.map(sensor => sensor.collisionCoordinate?.distance ?? 1.0));
+                actor.setSensorsPosition();
+
                 for (let index = 0; index < actor.sensors.length; index++) {
                     const sensor = actor.sensors[index];
 
@@ -234,18 +153,59 @@ export default class PlaygroundService {
                             sensor.setCollision(intersection?.x, intersection?.y, intersection?.z)
                             graphics.strokeCircleShape(sensor.collisionCircle);
                             break;
-                        }
-                        else sensor.reset()
+                        } else sensor.reset();
                     }
                 }
+
+                drawActor(actor);
+                calculateFitness(actor);
+            });
+
+            var allDead = false;
+            for (let index = 0; index < actors.length; index++) {
+                const actor = actors[index];
+                if (actor.isAlive === false) {
+                    allDead = true;
+                }
+                else {
+                    allDead = false;
+                    break;
+                }
+            }
+
+            if (allDead) generateNewPopulation();
+        }
+
+        function drawActor(actor) {
+            graphics.strokeCircleShape(actor.actorObject);
+            actor.sensors.forEach(sensor => {
+                graphics.strokeLineShape(sensor.sensorLine);
             });
         }
 
-        function createKeyboardInputs(input) {
-            keyUp = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-            keyDown = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-            keyLeft = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-            keyRight = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        function calculateFitness(actor) {
+            var currentDistance = Phaser.Math.Distance.Between(
+                actor.x, actor.y,
+                finishLine.x1, finishLine.y2 - (finishLine.y2 - finishLine.y1) / 2);
+            actor.fitness = 1 - currentDistance / maxDistance;
+        }
+
+        function hangleKeyboardInputs() {
+            if (keySpace.isDown) {
+                actors.forEach(actor => {
+                    actor.isAlive = false;
+                });
+            }
+            if (keyUp.isDown) {
+                var translationVector = new Phaser.Math.Vector2(1, 1);
+                actors[0].translateActor(translationVector);
+            }
+            if (keyLeft.isDown) {
+                actors[0].rotate(actors[0].angle - 1);
+            }
+            if (keyRight.isDown) {
+                actors[0].rotate(actors[0].angle + 1);
+            }
         }
     }
 }
