4caec8eddfe4cca160594398c36179afddcc3bc5
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-20 14:45:05 +0100

    Add initial implementation of PhenotypeFactory

---
+++ src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/Builders/PhenotypeFactory.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm.DataStructures;
+
+namespace Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm.Builders
+{
+    public class PhenotypeFactory<TAllele>
+    {
+        private readonly IAlleleFactory<TAllele> alleleFactory;
+
+        public PhenotypeFactory(IAlleleFactory<TAllele> alleleFactory)
+        {
+            this.alleleFactory = alleleFactory;
+        }
+
+        public Phenotype<TAllele> Build(int chromosomeSize, int genesSize)
+        {
+            return new Phenotype<TAllele>(
+                new Genotype<TAllele>(
+                    new List<Chromosome<TAllele>>(BuildChromosomes(chromosomeSize, genesSize))
+                )
+            );
+        }
+
+        private IEnumerable<Chromosome<TAllele>> BuildChromosomes(int chromosomeSize, int genesSize)
+        {
+            for (int i = 0; i < chromosomeSize; i++)
+            {
+                yield return new Chromosome<TAllele>(BuildGenes(genesSize));
+            }
+        }
+
+        private IEnumerable<Gene<TAllele>> BuildGenes(int genesSize)
+        {
+            for (int i = 0; i < genesSize; i++)
+            {
+                yield return new Gene<TAllele>(alleleFactory.GetValue());
+            }
+        }
+    }
+}
