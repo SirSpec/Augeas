01ca141c6cafd6dff568419908c35d90b19e0fa4
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-20 14:36:49 +0100

    Change name of generic argument in IFitnessFunction

--- src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/IFitnessFunction.cs
+++ src/server/Domain/ArtificialIntelligence/GenerticAlgorithm/IFitnessFunction.cs
@@ -1,8 +1,10 @@
+using Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm.DataStructures;
+
 namespace Hermes.Domain.ArtificialIntelligence.GenerticAlgorithm
 {
     // https://en.wikipedia.org/wiki/Fitness_function
     public interface IFitnessFunction
     {
-        double GetFitness<T>(Phenotype<T> phenotype);
+        double GetFitness<TAllele>(Phenotype<TAllele> phenotype);
     }
 }
