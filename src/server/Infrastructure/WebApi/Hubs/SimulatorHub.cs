c69190671e231fc41340d337ababffe006d7fffe
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-20 14:41:25 +0100

    Refactor implementation of SimulatorHub - Rename from CarHub to SimulatorHub - Adjust code to use Simulator

--- src/server/Infrastructure/WebApi/Hubs/SimulatorHub.cs
+++ src/server/Infrastructure/WebApi/Hubs/SimulatorHub.cs
@@ -3,25 +3,25 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Hermes.Infrastructure.WebApi
 {
-    public class CarHub : Hub
+    public class SimulatorHub : Hub
     {
-        private static Manager manager = new Manager();
+        private static Simulator simulator = new Simulator();
 
         public async Task GetAngle(int index, double[] sensors)
         {
-            var angle = manager.GetAngle(index, sensors);
+            var angle = simulator.GetAngle(index, sensors);
             await Clients.Caller.SendAsync("ReceiveAngle", index, angle);
         }
 
         public async Task SetFitness(int index, double fitness)
         {
-            manager.SetFitness(index, fitness);
+            simulator.SetFitness(index, fitness);
             await Clients.Caller.SendAsync("ReceiveConfirmation", true);
         }
 
         public async Task GenerateNewPopulation()
         {
-            manager.GenerateNewPopulation();
+            simulator.GenerateNewPopulation();
             await Clients.Caller.SendAsync("ReceiveNewPopulation", true);
         }
     }
