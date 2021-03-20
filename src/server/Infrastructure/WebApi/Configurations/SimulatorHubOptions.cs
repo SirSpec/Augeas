b9dfe94ff74220998b1c6551dd3ef45524bb0584
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-20 14:38:15 +0100

    Add implementation of SimulatorHubOptions

---
+++ src/server/Infrastructure/WebApi/Configurations/SimulatorHubOptions.cs
@@ -0,0 +1,9 @@
+namespace Hermes.Infrastructure.WebApi.Configurations
+{
+    public class SimulatorHubOptions
+    {
+        public const string SimulatorHub = "SimulatorHub";
+
+        public string? Url { get; set; }
+    }
+}
