fa670f13d229c3f9537bec4698dc5cb0b67f20c6
Spec <dawid.spec.jachowicz@gmail.com>
2021-03-24 18:25:20 +0100

    Remove redundant methods from ActorFactory

--- src/server/Infrastructure/WebApi/Hubs/ActorFactory.cs
+++ src/server/Infrastructure/WebApi/Hubs/ActorFactory.cs
@@ -1,9 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
-using Hermes.Domain.ArtificialIntelligence;
 using Hermes.Domain.ArtificialIntelligence.NeuralNetworks;
-using Hermes.Domain.ArtificialIntelligence.NeuralNetworks.Activations;
-using Hermes.Domain.ArtificialIntelligence.NeuralNetworks.Propagations;
 
 namespace Hermes.Infrastructure.WebApi
 {
@@ -14,28 +9,6 @@ namespace Hermes.Infrastructure.WebApi
             var build = new NeuralNetworkBuilder();
 
             return new Actor(build.Build(6, 1, 2));
-        }
-
-        private static IEnumerable<double> RandomWeight(int size)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                yield return Randomizer.RandomDouble(-1, 1);
-            }
-        }
-
-        private static Neuron GetNeuron(IEnumerable<INeuron> neurons, IEnumerable<double> weights)
-        {
-            var zipped = neurons.Zip(weights);
-
-            return new Neuron(
-                new WeightedSumFunction(),
-                new HyperbolicTangentFunction(),
-                zipped
-                    .Select(connection => new Connection(connection.First, connection.Second))
-                    .ToList(),
-                0
-            );
         }
     }
 }
