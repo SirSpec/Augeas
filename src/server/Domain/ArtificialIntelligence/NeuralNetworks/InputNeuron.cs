dc1d52e43cade9a05ea6e74b9607fe1adee5c385
Spec <dawid.spec.jachowicz@gmail.com>
2021-04-04 10:45:33 +0200

    Add Id implementation parameter to InputNeuron

--- src/server/Domain/ArtificialIntelligence/NeuralNetworks/InputNeuron.cs
+++ src/server/Domain/ArtificialIntelligence/NeuralNetworks/InputNeuron.cs
@@ -2,7 +2,11 @@ namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 {
 	public class InputNeuron : INeuron
 	{
+		public string Id { get; }
 		public double Input { get; set; }
+
+		public InputNeuron(string id) =>
+			Id = id;
 
 		public double Output => Input;
 	}
