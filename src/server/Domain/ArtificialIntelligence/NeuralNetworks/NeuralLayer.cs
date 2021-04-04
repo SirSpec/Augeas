daecefb469e2203cbdabf72d302e9bfbb94a16f2
Spec <dawid.spec.jachowicz@gmail.com>
2021-04-04 10:56:05 +0200

    Refactor NeuralLayer implementation - Add Get indexers - Secure constructor input

--- src/server/Domain/ArtificialIntelligence/NeuralNetworks/NeuralLayer.cs
+++ src/server/Domain/ArtificialIntelligence/NeuralNetworks/NeuralLayer.cs
@@ -1,12 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 {
 	public class NeuralLayer
 	{
-		public Neuron[] Neurons { get; }
+		private readonly Neuron[] neurons;
 
-		public NeuralLayer(params Neuron[] neurons)
-		{
-			Neurons = neurons;
-		}
+		public NeuralLayer(params Neuron[] neurons) =>
+			this.neurons = ContainsUniqueIds(neurons)
+				? neurons
+				: throw new ArgumentException($"{nameof(neurons)} set cannot contain duplicated Ids.");
+
+		public IEnumerable<Neuron> Neurons =>
+			neurons;
+
+		public Neuron this[int index] =>
+			neurons[index];
+
+		public Neuron this[string id] =>
+			Neurons.Single(neuron => neuron.Id == id);
+
+		private static bool ContainsUniqueIds(INeuron[] neurons) =>
+			neurons.GroupBy(neuron => neuron.Id).All(group => group.Count() == 1);
 	}
 }
