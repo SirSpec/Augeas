f9c7224b58ea14422219def6a820a9e210cce615
Spec <dawid.spec.jachowicz@gmail.com>
2021-05-26 22:11:15 +0200

    Refactor TensorExtensions

--- src/server/Domain/ArtificialIntelligence/TensorExtensions.cs
+++ src/server/Domain/ArtificialIntelligence/TensorExtensions.cs
@@ -4,18 +4,21 @@ namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 {
 	public static class TensorExtensions
 	{
+		private const int RowDimention = 0;
+		private const int ColumnDimention = 1;
+
 		public static Tensor MatrixMultiplicationBy(this Tensor first, Tensor second)
 		{
-			if (first.IsMatrix && second.IsMatrix && first.DopeVector.Shape[1] == second.DopeVector.Shape[0])
+			if (first.IsMatrix && second.IsMatrix && first.DopeVector.Shape[ColumnDimention] == second.DopeVector.Shape[RowDimention])
 			{
-				var resultRows = first.DopeVector.Shape[0];
-				var resultColumns = second.DopeVector.Shape[1];
+				var resultRows = first.DopeVector.Shape[RowDimention];
+				var resultColumns = second.DopeVector.Shape[ColumnDimention];
 
 				var result = new Tensor(resultRows, resultColumns);
 
 				for (int row = 0; row < resultRows; row++)
 					for (int column = 0; column < resultColumns; column++)
-						for (int i = 0; i < first.DopeVector.Shape[1]; i++)
+						for (int i = 0; i < first.DopeVector.Shape[ColumnDimention]; i++)
 							result[row, column] += first[row, i] * second[i, column];
 
 				return result;
@@ -27,10 +30,10 @@ namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 		{
 			if (tensor.IsMatrix)
 			{
-				var result = new Tensor(tensor.DopeVector.Shape[1], tensor.DopeVector.Shape[0]);
+				var result = new Tensor(tensor.DopeVector.Shape[ColumnDimention], tensor.DopeVector.Shape[RowDimention]);
 
-				for (int i = 0; i < tensor.DopeVector.Shape[1]; i++)
-					for (int j = 0; j < tensor.DopeVector.Shape[0]; j++)
+				for (int i = 0; i < tensor.DopeVector.Shape[ColumnDimention]; i++)
+					for (int j = 0; j < tensor.DopeVector.Shape[RowDimention]; j++)
 						result[i, j] = tensor[j, i];
 
 				return result;
