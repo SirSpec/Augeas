8c9e8024ed5cd47ddaec2ba19d79d9e90d57b732
Spec <dawid.spec.jachowicz@gmail.com>
2021-05-28 07:35:00 +0200

    Refactor Tensor - Define DopeVector as readonly field - Add Shape property

--- src/server/Domain/ArtificialIntelligence/Tensor.cs
+++ src/server/Domain/ArtificialIntelligence/Tensor.cs
@@ -1,28 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 {
 	public class Tensor
 	{
 		private readonly double[] values;
+		private readonly DopeVector dopeVector;
 
 		public Tensor(params int[] shape)
 		{
-			DopeVector = new DopeVector(shape);
-			values = new double[DopeVector.Length];
+			dopeVector = new DopeVector(shape);
+			values = new double[dopeVector.Length];
 		}
 
-		public DopeVector DopeVector { get; }
-		public bool IsMatrix => DopeVector.Rank == 2;
+		public IReadOnlyList<int> Shape => dopeVector.Shape;
+		public bool IsMatrix => dopeVector.Rank == 2;
 
 		public double this[params int[] indices]
 		{
-			get => indices.Length == DopeVector.Rank
+			get => indices.Length == dopeVector.Rank
 				? values[GetOffset(indices)]
-				: throw new ArgumentException($"Rank of {nameof(indices)} is not equal: {DopeVector.Rank}.");
-			set => values[GetOffset(indices)] = indices.Length == DopeVector.Rank
+				: throw new ArgumentException($"Rank of {nameof(indices)} is not equal: {dopeVector.Rank}.");
+			set => values[GetOffset(indices)] = indices.Length == dopeVector.Rank
 				? value
-				: throw new ArgumentException($"Rank of {nameof(indices)} is not equal: {DopeVector.Rank}.");
+				: throw new ArgumentException($"Rank of {nameof(indices)} is not equal: {dopeVector.Rank}.");
 		}
 
 		public static Tensor operator +(Tensor first, Tensor second) =>
@@ -39,17 +41,17 @@ namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 
 		private static Tensor PerformElementWiseOperation(Tensor first, Tensor second, Func<double, double, double> operation)
 		{
-			if (first.DopeVector.Rank == second.DopeVector.Rank)
+			if (first.dopeVector.Rank == second.dopeVector.Rank)
 			{
-				var result = new Tensor(first.DopeVector.Shape);
+				var result = new Tensor(first.dopeVector.Shape);
 
-				for (int i = 0; i < result.DopeVector.Length; i++)
+				for (int i = 0; i < result.dopeVector.Length; i++)
 					result.values[i] = operation(first.values[i], second.values[i]);
 
 				return result;
 			}
 			else throw new ArgumentException(
-				$"Inputs have different Ranks:{first.DopeVector.Rank}|{second.DopeVector.Rank}.");
+				$"Inputs have different Ranks:{first.dopeVector.Rank}|{second.dopeVector.Rank}.");
 		}
 
 		private int GetOffset(params int[] indices)
@@ -57,7 +59,7 @@ namespace Hermes.Domain.ArtificialIntelligence.NeuralNetworks
 			var index = 0;
 
 			for (int i = 0; i < indices.Length; i++)
-				index += indices[i] * DopeVector.Strides[i];
+				index += indices[i] * dopeVector.Strides[i];
 
 			return index;
 		}
